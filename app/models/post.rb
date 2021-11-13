10ce12219fcc5c67dd122ea494685c18285c6a45
okabekaito <kaito19991204@gmail.com>
2021-11-13 17:08:11 +0900

    validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,5 @@
 class Post < ApplicationRecord
+    validates :title,    length: { maximum: 20}, presence: true
+    validates :contents,    length: { maximum: 20}, presence: true
+    validates :time, numericality: true
 end
