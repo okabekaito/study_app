82cb4d59b10a30490a1d07f49fb5ff455c6e1599
okabekaito <kaito19991204@gmail.com>
2021-11-14 11:09:00 +0900

    test

---
+++ spec/factories/users.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :user do
+        email                 {"test12@gmail.com"}
+        password              {"aaBB123456"}
+        password_confirmation {"aaBB123456"}
+    end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  context "タイトルと内容が入力されている場合" do
+    let!(:post) do
+      user = FactoryBot.create(:user)
+      post = user.posts.build({title: "Rspecのテスト", contents:"Rspec！",time:1})
+    end
+    it '記録を保存できる' do
+      expect(post).to be_valid
+    end
+  end
+end
