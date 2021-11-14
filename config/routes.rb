57ba371cf4d3c991e83a469f0d9c51cf3b826058
okabekaito <kaito19991204@gmail.com>
2021-11-14 09:21:48 +0900

    guest

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  root'hellos#index'
   resources :posts
-  root'posts#index'
+  post '/hellos/guest_sign_in', to: 'hellos#guest_sign_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
