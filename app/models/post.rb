5b41405eab50fcc8bbded3eb881fdd9889e4af87
okabekaito <kaito19991204@gmail.com>
2021-11-16 10:58:36 +0900

    validate500

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     validates :title,    length: { maximum: 50}, presence: true
-    validates :contents,    length: { maximum: 50}, presence: true
+    validates :contents,    length: { maximum: 500}, presence: true
     validates :time, numericality: true
 
     belongs_to :user
